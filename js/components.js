b14c199eb9e062c7cf68275e0dd15421bb697e46
“Calixto” <l19070031@huetamo.tecnm.mx>
2025-05-06 19:41:00 -0600

    Ajustes en clases

--- js/components.js
+++ js/components.js
@@ -3,16 +3,6 @@ $(document).ready(function () {
         closeOnclick: true
     });
 
-
-    // $('.carousel').carousel();
-
-    // $('.carousel.carousel-slider').carousel(
-    //     {
-    //         fullWidth: true,
-    //         indicators: true,
-    //     }
-    // );
-
     $('.carousel.carousel-slider').carousel(
         {
             fullWidth: true,
@@ -32,40 +22,3 @@ $(document).ready(function () {
 
 
 });
-
-
-// const slides = document.querySelectorAll('.carousel-item');
-// let currentSlide = 0;
-// const totalSlides = slides.length;
-
-// function showSlide(index) {
-//     slides.forEach((slide, i) => {
-//         slide.classList.add("fade-in");
-//         slide.style.display = i === index ? 'block' : 'none';
-//     });
-// }
-// function nextSlide() {
-//     currentSlide = (currentSlide + 1) % totalSlides;
-//     showSlide(currentSlide);
-// }
-
-// showSlide(currentSlide);
-
-// setInterval(nextSlide, 5500);
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     const elems = document.querySelectorAll('.carousel');
-//     const instances = M.Carousel.init(elems, {
-//         fullWidth: true,
-//         indicators: true,
-//         duration: 200,
-//         interval: 3000 // Cambia cada 3 segundos
-//     });
-
-//     const imgElement = document.getElementById("img");
-//     if (imgElement) {
-//         imgElement.style.width = "768px"; // Cambia el ancho de la imagen
-//     } else {
-//         console.error("El elemento con ID 'img' no se encontró.");
-//     }
-// });
